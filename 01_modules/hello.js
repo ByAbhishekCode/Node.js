b33ee0b26ac3bfd9747a367cccd838cd110d822b
Abhishek <abhikumar1260@gmail.com>
2025-06-28 16:54:02 +0530

    sync and aync file also thead working of nodejs

---
+++ 01_modules/hello.js
@@ -0,0 +1,5 @@
+const {add,sub} = require("./math")
+// const math = require("./math")
+console.log('hi')
+console.log("this is " + add(2,5)  + " sub is "  + sub(4,4))
+
---
+++ 02_fileHandling/file.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+const os = require('os')
+
+// fs.writeFileSync("./test.js","console.log('hi')")
+
+console.log(os.cpus().length)
