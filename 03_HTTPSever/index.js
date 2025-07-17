93da3c2a8d1719746d77aff932da622a611f4a47
Abhishek <abhikumar1260@gmail.com>
2025-07-17 17:10:14 +0530

    03 something updated

--- 03_HTTPSever/index.js
+++ 03_HTTPSever/index.js
@@ -1,22 +1,21 @@
 const http = require("http");
 const fs = require("fs");
-
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: ${req.url} New Req Recevied\n`;
+  const log = `${Date.now()} and Path is:-> ${req.url} <-: New Req Recevied\n`;
   fs.appendFile("log.txt", log, (err, data) => {
-    switch(req.url){
-    case '/' : res.end("Home Page")
-    break
-    case '/about' : res.end("About page")
-    break
-    case '/contact' : res.end('contact')
-    break
-    default: req.end('404 server error')
+    switch (req.url) {
+      case "/":
+        res.end("Hello From Home");
+        break;
+      case "/about":
+        res.end("Hello From Abhishek form about");
+        break;
+      default:
+        res.end("404 Not Found");
     }
-    res.end("Hello From sever");
   });
+  console.log("New Request Recived");
 });
 
-myServer.listen(1234, () => {
-  console.log("server here started at:-> 1234 ");
-});
+myServer.listen(8000, () => console.log("Server Started..."));
+
